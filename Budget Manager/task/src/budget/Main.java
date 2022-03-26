3c92fadc229d2f06a4e0ca1ae6ac94c0de18a1bb
ahmedhosnypro <80482137+ahmedhosnypro@users.noreply.github.com>
2022-03-26 23:00:17 +0200

    Oh the things you can buy

--- Budget Manager/task/src/budget/Main.java
+++ Budget Manager/task/src/budget/Main.java
@@ -1,13 +1,16 @@
 package budget;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.TreeMap;
 
 public class Main {
     private static final Scanner scanner = new Scanner(System.in);
     private static double balance = 0;
-    private static final TreeMap<String, Double> purchases = new TreeMap<>();
+    private static final TreeMap<String, Double> allPurchases = new TreeMap<>();
+    private static final TreeMap<String, Double> foodPurchases = new TreeMap<>();
+    private static final TreeMap<String, Double> clothesPurchases = new TreeMap<>();
+    private static final TreeMap<String, Double> entertainmentPurchases = new TreeMap<>();
+    private static final TreeMap<String, Double> otherPurchases = new TreeMap<>();
 
     public static void main(String[] args) {
         startAction();
@@ -15,7 +18,7 @@ public class Main {
 
     private static void startAction() {
         System.out.println("""
-                
+                                
                 Choose your action:
                 1) Add income
                 2) Add purchase
@@ -48,13 +51,46 @@ public class Main {
     }
 
     private static void addPurchase() {
+        System.out.println("""
+                
+                Choose the type of purchase
+                1) Food
+                2) Clothes
+                3) Entertainment
+                4) Other
+                5) Back""");
+
+        int purchaseType = Integer.parseInt(scanner.nextLine());
+        System.out.println();
+
+        switch (purchaseType) {
+            case 1 -> addPurchaseUtility("Food");
+            case 2 -> addPurchaseUtility("Clothes");
+            case 3 -> addPurchaseUtility("Entertainment");
+            case 4 -> addPurchaseUtility("Other");
+            case 5 -> startAction();
+            default -> throw new IllegalArgumentException();
+        }
+    }
+
+    private static void addPurchaseUtility(String purchaseType) {
+
         System.out.println("Enter purchase name:");
         String purchaseName = scanner.nextLine();
 
         System.out.println("Enter its price:");
         double purchaseCost = Double.parseDouble(scanner.nextLine());
 
-        purchases.put(purchaseName, purchaseCost);
+        allPurchases.put(purchaseName, purchaseCost);
+        switch (purchaseType) {
+            case "Food" -> foodPurchases.put(purchaseName, purchaseCost);
+            case "Clothes" -> clothesPurchases.put(purchaseName, purchaseCost);
+            case "Entertainment" -> entertainmentPurchases.put(purchaseName, purchaseCost);
+            case "Other" -> otherPurchases.put(purchaseName, purchaseCost);
+            default -> throw new IllegalArgumentException();
+        }
+
+        System.out.println("Purchase was added!");
 
         if (purchaseCost > 0) {
             if (balance - purchaseCost > 0) {
@@ -64,20 +100,52 @@ public class Main {
             }
         }
 
-        startAction();
+        addPurchase();
     }
 
     private static void showPurchases() {
+        System.out.println("""
+                
+                Choose the type of purchases
+                1) Food
+                2) Clothes
+                3) Entertainment
+                4) Other
+                5) All
+                6) Back""");
+        int purchaseType = Integer.parseInt(scanner.nextLine());
+        System.out.println();
+
+        switch (purchaseType) {
+            case 1 -> showPurchasesUtility(foodPurchases);
+            case 2 -> showPurchasesUtility(clothesPurchases);
+            case 3 -> showPurchasesUtility(entertainmentPurchases);
+            case 4 -> showPurchasesUtility(otherPurchases);
+            case 5 -> showPurchasesUtility(allPurchases);
+            case 6 -> startAction();
+            default -> throw new IllegalArgumentException();
+        }
+    }
+
+    private static void showPurchasesUtility(TreeMap<String, Double> purchases) {
         if (purchases.isEmpty()) {
             System.out.println("The purchase list is empty");
+            if (allPurchases.isEmpty()) {
+                startAction();
+            } else {
+                showPurchases();
+            }
         } else {
             for (var purchase : purchases.entrySet()) {
                 System.out.printf("%s: $%2f%n", purchase.getKey(), purchase.getValue());
             }
-            System.out.printf("Balance: $%2f%n", balance);
+            System.out.printf("Total sum: $%2f%n", purchases.values().stream()
+                    .reduce(0.0, Double::sum));
+
+            showPurchases();
         }
-        startAction();
     }
+
 
     private static void showBalance() {
         System.out.printf("Balance: $%2f%n", balance);
