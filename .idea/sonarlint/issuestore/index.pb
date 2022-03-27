5548613906d57b483aebf34d8a08ee540d2d434f
ahmedhosnypro <80482137+ahmedhosnypro@users.noreply.github.com>
2022-03-27 22:04:35 +0200

    Analyzer

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,40 @@
+
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+\
+,Budget Manager/Count my money/task-info.yaml,c\9\c9948ba16d9db746a96b29065e86a996dad5ad86
+à
+¯.idea/modules/Budget Manager/task/build/generated/sources/annotationProcessor/java/Budget_Manager.Budget_Manager-task-build-generated-sources-annotationProcessor-java.test.iml,0\e\0eae7a36734f711dcfa1e0b44a09d7e7b10cf7df
+È
+—.idea/modules/Budget Manager/task/build/generated/sources/headers/java/Budget_Manager.Budget_Manager-task-build-generated-sources-headers-java.test.iml,1\a\1ad535261ca1acb31a4340edbaeb630ea9b2ae95
+¥
+u.idea/modules/Budget Manager/task/build/reports/tests/Budget_Manager.Budget_Manager-task-build-reports-tests.test.iml,5\1\517698e172b4d2ac882c76ab23b2635af19c5c29
+£
+s.idea/modules/Budget Manager/task/build/test-results/Budget_Manager.Budget_Manager-task-build-test-results.test.iml,d\d\dd09c4ed19106ffa972c864d7650e69de78518df
+‘
+a.idea/modules/Budget Manager/task/build/tmp/Budget_Manager.Budget_Manager-task-build-tmp.test.iml,f\6\f644c68e19728793d1e25f04354f7836fabe33f8
+
+n.idea/modules/Budget Manager/task/build/classes/java/Budget_Manager.Budget_Manager-task-build-classes-java.iml,7\3\73b4157112f75df8b153dd979c22f88b49f7341f
+Û
+ª.idea/modules/Budget Manager/task/build/generated/sources/annotationProcessor/java/Budget_Manager.Budget_Manager-task-build-generated-sources-annotationProcessor-java.iml,e\9\e97349d46a1757012e798de8fcbfc276303c4302
+Ã
+’.idea/modules/Budget Manager/task/build/generated/sources/headers/java/Budget_Manager.Budget_Manager-task-build-generated-sources-headers-java.iml,c\c\ccfd76ea5a93caba4aebb2e23e047a1af5402ad7
+ 
+p.idea/modules/Budget Manager/task/build/reports/tests/Budget_Manager.Budget_Manager-task-build-reports-tests.iml,b\c\bc9e17ad36d3b5e75bc1e10a51c036c8dd5506e9
+
+n.idea/modules/Budget Manager/task/build/test-results/Budget_Manager.Budget_Manager-task-build-test-results.iml,0\1\0167fecd252cb8676a21b7637ca0bca513e61a23
+£
+s.idea/modules/Budget Manager/task/build/classes/java/Budget_Manager.Budget_Manager-task-build-classes-java.test.iml,f\d\fd798e27a3fdb8f2fe72eefe1951665e511f34fe
+Œ
+\.idea/modules/Budget Manager/task/build/tmp/Budget_Manager.Budget_Manager-task-build-tmp.iml,c\a\cade1ff0bb3a61b31dfbfcf1084ec26e8fd427c7
+Y
+)Budget Manager/Make a menu/task-info.yaml,b\7\b75fe0f8ea7921eb7019e6028d5af4643d5ea320
+g
+7Budget Manager/Oh the things you can buy/task-info.yaml,7\9\7952f2904190ae85c7f1a3cf1f02d1c49f3447e2
+`
+0Budget Manager/task/src/budget/PurchaseType.java,f\a\fac47b68254f13a61e35c3d3dcd4a7d2868db809
+a
+1Budget Manager/Memorable purchases/task-info.yaml,8\1\81bc3e68dfe9e7c22e8115633dbb8bccf2bed1c4
+Q
+!Budget Manager/task/purchases.txt,9\e\9e8ee27568c34b408c821382517989f7f5e819be
--- Budget Manager/task/src/budget/Main.java
+++ Budget Manager/task/src/budget/Main.java
@@ -4,8 +4,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
+import java.util.function.Consumer;
 
 public class Main {
     private static final Scanner scanner = new Scanner(System.in);
@@ -18,6 +18,13 @@ public class Main {
     private static final File saveFile = new File(SAVE_PATH);
 
     private static final String EMPTY_MESSAGE = "The purchase list is empty";
+
+    private static final Consumer<Purchase> purchasePrinter = p ->
+            System.out.printf("%s $%.2f%n", p.getName(), p.getPrice());
+
+    private static final Comparator<Purchase> purchaseComparator = (p1, p2) ->
+            Double.compare(p2.getPrice(), p1.getPrice());
+
 
     public static void main(String[] args) {
         startAction();
@@ -33,6 +40,7 @@ public class Main {
                 4) Balance
                 5) Save
                 6) Load
+                7) Analyze (Sort)
                 0) Exit""");
 
         int action = Integer.parseInt(scanner.nextLine());
@@ -45,7 +53,11 @@ public class Main {
             case 4 -> showBalance();
             case 5 -> save();
             case 6 -> load();
-            case 0 -> System.out.println("Bye!");
+            case 7 -> sort();
+            case 0 -> {
+                System.out.println("Bye!");
+                System.exit(0);
+            }
             default -> throw new IllegalArgumentException();
         }
     }
@@ -139,9 +151,10 @@ public class Main {
             if (purchases.stream().noneMatch(p -> p.getType().equals(purchaseType))) {
                 System.out.println(EMPTY_MESSAGE);
             } else {
-                purchases.stream().filter(p -> p.getType().equals(purchaseType)).forEach(p -> System.out.printf("%s $%.2f%n", p.getName(), p.getPrice()));
-                System.out.printf("Total sum: $%.2f%n", purchases.stream().filter(p -> p.getType().equals(purchaseType)).mapToDouble(Purchase::getPrice).reduce(0.0, Double::sum));
-
+                purchases.stream()
+                        .filter(p -> p.getType().equals(purchaseType))
+                        .forEach(purchasePrinter);
+                System.out.printf("Total sum: $%.2f%n", getTotal(purchaseType));
             }
             showPurchases();
         }
@@ -152,8 +165,8 @@ public class Main {
             System.out.println(EMPTY_MESSAGE);
             startAction();
         } else {
-            purchases.forEach(p -> System.out.printf("%s $%.2f%n", p.getName(), p.getPrice()));
-            System.out.printf("Total sum: $%.2f%n", purchases.stream().mapToDouble(Purchase::getPrice).reduce(0.0, Double::sum));
+            purchases.forEach(purchasePrinter);
+            System.out.printf("Total sum: $%.2f%n", getTotal());
         }
         showPurchases();
     }
@@ -191,9 +204,7 @@ public class Main {
             while (fileScanner.hasNext()) {
                 String purchase = fileScanner.nextLine();
 
-                purchases.add(new Purchase(purchase.substring(0, purchase.lastIndexOf("$") - 1),
-                        Double.parseDouble(purchase.substring(purchase.lastIndexOf("$") + 1, purchase.lastIndexOf(" ")))
-                        , PurchaseType.valueOf(purchase.substring(purchase.lastIndexOf("#") + 1))));
+                purchases.add(new Purchase(purchase.substring(0, purchase.lastIndexOf("$") - 1), Double.parseDouble(purchase.substring(purchase.lastIndexOf("$") + 1, purchase.lastIndexOf(" "))), PurchaseType.valueOf(purchase.substring(purchase.lastIndexOf("#") + 1))));
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -201,5 +212,107 @@ public class Main {
 
         System.out.println("Purchases were loaded!");
         startAction();
+    }
+
+    private static void sort() {
+        System.out.println("""
+                                
+                How do you want to sort?
+                1) Sort all purchases
+                2) Sort by type
+                3) Sort certain type
+                4) Back""");
+
+        int sorting = Integer.parseInt(scanner.nextLine());
+        System.out.println();
+
+        switch (sorting) {
+            case 1 -> sortAll();
+            case 2 -> sortByType();
+            case 3 -> sortCertainType();
+            case 4 -> startAction();
+            default -> throw new IllegalArgumentException();
+        }
+    }
+
+    private static void sortAll() {
+        if (purchases.isEmpty()) {
+            System.out.println(EMPTY_MESSAGE);
+        } else {
+            purchases.stream()
+                    .sorted(purchaseComparator)
+                    .forEach(purchasePrinter);
+            System.out.printf("Total: $%.2f%n", getTotal());
+        }
+        sort();
+    }
+
+    private static void sortByType() {
+        double foodTotal = getTotal(PurchaseType.FOOD);
+        double clothesTotal = getTotal(PurchaseType.CLOTHES);
+        double entertainmentTotal = getTotal(PurchaseType.ENTERTAINMENT);
+        double otherTotal = getTotal(PurchaseType.OTHER);
+
+
+        ArrayList<Purchase> totals = new ArrayList<>(
+                List.of(new Purchase("Food -", foodTotal, PurchaseType.FOOD),
+                        new Purchase("Clothes -", clothesTotal, PurchaseType.CLOTHES),
+                        new Purchase("Entertainment -", entertainmentTotal, PurchaseType.ENTERTAINMENT),
+                        new Purchase("Other -", otherTotal, PurchaseType.OTHER)));
+
+        totals.sort(purchaseComparator);
+        totals.forEach(purchasePrinter);
+        System.out.printf("Total sum: $%.2f%n", getTotal());
+
+        sort();
+    }
+
+    private static void sortCertainType() {
+        System.out.println("""
+                                
+                Choose the type of purchase
+                1) Food
+                2) Clothes
+                3) Entertainment
+                4) Other""");
+
+        int purchaseType = Integer.parseInt(scanner.nextLine());
+        System.out.println();
+
+        switch (purchaseType) {
+            case 1 -> sortCertainTypeUtility(PurchaseType.FOOD);
+            case 2 -> sortCertainTypeUtility(PurchaseType.CLOTHES);
+            case 3 -> sortCertainTypeUtility(PurchaseType.ENTERTAINMENT);
+            case 4 -> sortCertainTypeUtility(PurchaseType.OTHER);
+            default -> throw new IllegalArgumentException();
+        }
+
+        sort();
+    }
+
+    private static void sortCertainTypeUtility(PurchaseType purchaseType) {
+        if (purchases.stream().noneMatch(p -> p.getType().equals(purchaseType))) {
+            System.out.println(EMPTY_MESSAGE);
+        } else {
+            purchases.stream()
+                    .filter(p -> p.getType().equals(purchaseType))
+                    .sorted(purchaseComparator)
+                    .forEach(purchasePrinter);
+            System.out.printf("Total sum: $%.2f%n", getTotal(purchaseType));
+        }
+        sort();
+    }
+
+    private static double getTotal(PurchaseType purchaseType) {
+        return purchases.stream()
+                .filter(p -> p.getType().equals(purchaseType))
+                .mapToDouble(Purchase::getPrice)
+                .reduce(0.0, Double::sum);
+    }
+
+    private static double getTotal() {
+        return purchases.stream()
+                .mapToDouble(Purchase::getPrice)
+                .reduce(0.0, Double::sum);
     }
 }
